206075b5c9e88936573f3c416037dbe0a99d73df
Omkar Mirji <omkar.mirji@gmail.com>
2024-05-15 15:38:02 +0530

    creating_output.csv

--- training_script.py
+++ training_script.py
@@ -9,10 +9,9 @@ import numpy as np
 import pandas as pd
 import joblib
 import os 
-#os.chdir("C:\OmkaRucha\Local Disk_D\Inttrvu_ai\ML project example-20240427T060302Z-001\ML project example")
 
 ### Step1: Data Gathering and Loading 
-df=pd.read_csv("C:\OmkaRucha\Local Disk_D\Inttrvu_ai\Automotive_Engine_Health_Prediction\WIP\engine_data.csv")
+df=pd.read_csv("C:\OmkaRucha\Local Disk_D\Inttrvu_ai\Automotive_Engine_Health_Prediction\WIP\Automotive_Health_Monitering\engine_data.csv")
 #df.head(5)
 
 ### Importing Model_preprocessing.py to carry out EDA
@@ -25,16 +24,14 @@ multivariate_analysis(df)
 
 ### Train/Dev split (to prevent Data Leakage)
 
-data_train = df[0:15627]
+# data_train = df[0:15627]
 
-y = data_train['Engine Condition']
-x = data_train.drop(['Engine Condition'],axis = 1)
+y = df['Engine Condition']
+x = df.drop(['Engine Condition'],axis = 1)
 
 ### Train/Test Split for Model Building
 
 from Model_Bulding import train_test_split_and_features
-
-#x_, x_dev, y_, y_dev = train_test_split_and_features(df,x,y,0.1)
 
 x_train, x_test, y_train, y_test = train_test_split_and_features(df,x,y,0.2)
 
@@ -45,7 +42,7 @@ from Model_Bulding import random_forest_model, decision_tree_model,fit_model,fea
 rf_model  = random_forest_model()
 dt_model = decision_tree_model()
 
-model = fit_model(rf_model, x_train, y_train)
+model = fit_model(dt_model, x_train, y_train)
 
 # Prediction on y_test
 
