eaebf31b67222c4d885058e3dfeb02525b42d05c
omkarmirji <70942288+omkarmirji@users.noreply.github.com>
2024-05-18 21:48:33 +0530

    Updated

--- Model_Preprocessing.py
+++ Model_Preprocessing.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # -*- coding: utf-8 -*-
 """
 Created on Sat Apr 27 18:52:13 2024
@@ -36,42 +36,4 @@ def multivariate_analysis(df):
         sns.boxplot(y = col_name , x= 'Engine Condition', hue='Engine Condition', data=df)
     plt.show()
 
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 27 18:52:13 2024
 
-@author: omkar
-"""
-
-# Importing the Libraries
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-## EDA --> Exploring the data and figuring out Missing values in the Data
-
-def preprocessing(df):
-    print(df.info())
-    print(df.isnull().sum())
-    print(df.describe())
-    print(df['Engine Condition'].value_counts())
-
-def univariate_analysis(df):
-    plt.figure(figsize = (20,12))
-    list_columns =  df.columns[0:6].tolist()
-    for index, col_name in enumerate(list_columns):
-        plt.subplot(2,3,index+1)
-        sns.kdeplot(x= col_name, hue='Engine Condition', data=df)
-    plt.show()
-
-def multivariate_analysis(df):
-    plt.figure(figsize = (20,12))
-    list_columns =  df.columns[0:6].tolist()
-    for index, col_name in enumerate(list_columns):
-        plt.subplot(2,3,index+1)
-        sns.boxplot(y = col_name , x= 'Engine Condition', hue='Engine Condition', data=df)
-    plt.show()
-
->>>>>>> 41293da5528830a7fe61c18a1b03b6c82d1ae2d4
--- Prediction_Dev_Set.py
+++ Prediction_Dev_Set.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # -*- coding: utf-8 -*-
 """
 Created on Sat Apr 27 23:32:13 2024
@@ -9,9 +9,9 @@ Created on Sat Apr 27 23:32:13 2024
 import numpy as np
 import pandas as pd
 import joblib
-#from prediction import predict_model,evalute_model
+from prediction import predict_model,evalute_model
 
-prediction_file_path="C:\OmkaRucha\Local Disk_D\Inttrvu_ai\Automotive_Engine_Health_Prediction\WIP\Automotive_Health_Monitering\Predictions.csv"
+prediction_file_path="C:\Omkar\Work\DS\ML\Automotive_Health_Prediction_Analysis-main\Predictions.csv"
 
 df_predict=pd.read_csv(prediction_file_path)
 
@@ -32,38 +32,3 @@ predictions_output.to_csv("output.csv")
 
 
 
-
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 27 23:32:13 2024
-
-@author: omkar
-"""
-
-import numpy as np
-import pandas as pd
-import joblib
-from prediction import predict_model,evalute_model
-
-#df=pd.read_csv("C:\OmkaRucha\Local Disk_D\Inttrvu_ai\Automotive_Engine_Health_Prediction\WIP\Automotive_Health_Monitering\engine_data.csv")
-
-#data_dev = df[15627:]
-
-#y_dev = data_dev['Engine Condition']
-#x_dev = data_dev.drop(['Engine Condition'],axis = 1)
-
-
-x_check = np.array([845,4.877239363,3.669304,3.504418,76.30162614,70.4]).reshape(1,-1)
-
-model = joblib.load('model_classifier.pkl')
-
-
-output = predict_model(model, x_check)
-#evalute_model(y_dev ,predictions)
-print(output)
-
-
-
-
->>>>>>> 41293da5528830a7fe61c18a1b03b6c82d1ae2d4
